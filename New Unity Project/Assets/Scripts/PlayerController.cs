35522b063fee78ab4abd0db580031b0f2153e5b7
Skilious <onlinegamingsocial@gmail.com>
2021-05-16 10:57:14 +1000

    Update: Added crouching Fixed camera sensitivity settings.

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private CinemachineVirtualCamera vcam;
+    public CinemachineTransposer camTransposer;
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
-    private float gravityValue = -9.81f;
+    protected float playerSpeed = 0.0f;
+    private readonly float crouchSpeed = 1.0f;
+    private readonly float walkSpeed = 50.0f;
+    private readonly float gravityValue = -9.81f;
 
     private InputManager inputManager;
     private Transform cameraTransform;
+
+    //Height value when crouched.
+    private float crouchHeight = 1.0f;
+    //Height value when standing.
+    private float defaultHeight = 2.0f;
+
+    public bool actionTriggered = false;
     private void Start()
     {
+        camTransposer = vcam.GetCinemachineComponent<CinemachineTransposer>();
+        playerSpeed = walkSpeed;
         cameraTransform = Camera.main.transform;
         inputManager = InputManager.instance;
         controller = GetComponent<CharacterController>();
@@ -38,5 +52,37 @@ public class PlayerController : MonoBehaviour
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+
+        if (inputManager.PlayerCrouched())
+        {
+            if (actionTriggered)
+            {
+                actionTriggered = false;
+            }
+            else
+                actionTriggered = true;
+        }
+
+        if (actionTriggered)
+        {
+            print("Crouching");
+            controller.height = Mathf.MoveTowards(controller.height, crouchHeight, Time.deltaTime / 0.5f);
+
+            playerSpeed = crouchSpeed;
+        }
+        else if (!actionTriggered)
+        {
+            print("Standing");
+            controller.height = Mathf.MoveTowards(controller.height, defaultHeight, Time.deltaTime / 0.5f); 
+            playerSpeed = walkSpeed;
+        }
+        camTransposer.m_FollowOffset.y = actionTriggered ? HeightChange(-1.0f) : HeightChange(0.0f);
+        controller.center = Vector3.down * (defaultHeight - controller.height) / 2.0f;
+    }
+
+    private float HeightChange(float N)
+    {
+        float results = Mathf.MoveTowards(camTransposer.m_FollowOffset.y, N, Time.deltaTime / 0.5f);
+        return results;
     }
 }
